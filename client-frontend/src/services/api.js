d7aba4f8da2e140eb64f788604de4a3ee41de914
Omar Lengua <omarns21@gmail.com>
2025-03-06 22:12:36 +0100

    Configuring docker-compose

--- client-frontend/src/services/api.js
+++ client-frontend/src/services/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'http://localhost:8000/api/tasks',
 });
 
 export const fetchTasks = async () => {
   try {
-    const response = await api.get('/tasks');
+    const response = await api.get('/');
     return response.data;
   } catch (error) {
     console.error('Error fetching tasks:', error);
