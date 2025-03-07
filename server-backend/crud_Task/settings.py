259245a36130c9648ced7d564d6623a0c176fec5
Omar Lengua <omarns21@gmail.com>
2025-03-07 01:02:25 +0100

    Implement: microservice, working

--- server-backend/crud_Task/settings.py
+++ server-backend/crud_Task/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,7 +84,8 @@ DATABASES = {
         'NAME': 'crud_task',
         'USER': 'root',
         'PASSWORD': '123412',
-        'HOST': 'localhost',
+        # 'HOST': 'localhost',
+        'HOST': os.getenv('DB_HOST'),
         'PORT': '3306',
     }
 }
